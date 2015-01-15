c8c9d547584275bb7428fb7582c8c227388b0c59
mchsh <chuenshengm@gmail.com>
2015-01-15 21:39:40 +0800

    with shared memory

--- main.cc
+++ main.cc
@@ -9,15 +9,38 @@ int main()
 		printf("generate matrix file error\n");
 	int *matrix;
 	int *result;
-	int i;
+	int i, j;
 	matrix=(int*)malloc(Row*Row*sizeof(int));
 	result=(int*)malloc(Row*Row*sizeof(int));
 	matrix=readMtxFile("matrix.txt", Row, Row);
+
+	fprintf(stdout, "\nthe matrix:\n");
+	for(i=0; i<Row; i++){
+		for(j=0; j<Row; j++){
+			fprintf(stdout, "%d ", matrix[i*Row + j]);
+		}
+		fprintf(stdout, "\n");
+	}
+
+	fprintf(stdout, "\nthe device output:\n");
 	mtxMultCUDA(matrix, matrix, result, Row);
-//	mtxMultiplication(matrix, matrix, result, 4);
-//	result=readMtxFile("results.txt", 4, 4);
-//	for(i=0; i<16; i++){
-//		printf("%d ", result[i]);
-//	}
+	// print out results
+	for(i=0; i<Row; i++){
+		for(j=0; j<Row; j++){
+			fprintf(stdout, "%d ", result[i*Row + j]);
+		}
+		fprintf(stdout, "\n");
+	}
+
+	// for comparison
+	mtxMultiplication(matrix, matrix, result, Row);
+	fprintf(stdout, "\nthe host version:\n");
+	for(i=0; i<Row; i++){
+		for(j=0; j<Row; j++){
+			fprintf(stdout, "%d ", result[i*Row + j]);
+		}
+		fprintf(stdout, "\n");
+	}
+
 	return(0);
 }
--- mtxMultiplication.cu
+++ mtxMultiplication.cu
@@ -27,12 +27,13 @@ void mtxMultiplication(int *m, int *n, int *p, int rows)
 			}
 		}
 	}
-	writeMtxFile("results.txt", p, rows, rows);
+//	writeMtxFile("results.txt", p, rows, rows);
 	if(isNULL)
 		free(p);
 }
 
 __global__ void mtxMultKernel(int *pmDev, int *pnDev, int *ppDev, int tileRow, int mtxRow);
+__global__ void mtxMultKernelGlobal(int *pmDev, int *pnDev, int *ppDev, int tileRow, int mtxRow);
 
 // function port to cuda device
 void mtxMultCUDA(int *pm, int *pn, int *pp, int rows)
@@ -43,7 +44,7 @@ void mtxMultCUDA(int *pm, int *pn, int *pp, int rows)
 	cudaMalloc((void**)&pmDev, size);
 	cudaMalloc((void**)&pnDev, size);
 	cudaMalloc((void**)&ppDev, size);
-	// copy content of the matrix from host to device
+	// copy content of the matrix from host RAM to device global memory
 	cudaMemcpy(pmDev, pm, size, cudaMemcpyHostToDevice);
 	cudaMemcpy(pnDev, pn, size, cudaMemcpyHostToDevice);
 
@@ -53,6 +54,7 @@ void mtxMultCUDA(int *pm, int *pn, int *pp, int rows)
 	dim3 dimGrid(4,4);
 	// 2.lunch the kernel
 	mtxMultKernel<<<dimGrid, dimBlock>>>(pmDev, pnDev, ppDev, 4, rows);
+//	mtxMultKernelGlobal<<<dimGrid, dimBlock>>>(pmDev, pnDev, ppDev, 4, rows);
 	
 	// copy content of the result matrix form device to host
 	cudaMemcpy(pp, ppDev, size, cudaMemcpyDeviceToHost);
@@ -60,31 +62,10 @@ void mtxMultCUDA(int *pm, int *pn, int *pp, int rows)
 	cudaFree(ppDev);
 	cudaFree(pmDev);
 	cudaFree(pnDev);
-
-	// print out
-	int i=0;
-	int j=0;
-	for(i=0; i<rows; i++){
-		for(j=0; j<rows; j++){
-			fprintf(stdout, "%d ", pp[i*rows + j]);
-		}
-		fprintf(stdout, "\n");
-	}
-	fprintf(stdout, "\nthe matrix:\n");
-	for(i=0; i<rows; i++){
-		for(j=0; j<rows; j++){
-			fprintf(stdout, "%d ", pm[i*rows + j]);
-		}
-		fprintf(stdout, "\n");
-	}
-/*	for(i=0; i<rows*rows; i++){
-		fprintf(stdout, "%d ", pm[i]);
-	}
-*/
 }
 
-// matrix multipliction kernel function
-__global__ void mtxMultKernel(int *pmDev, int *pnDev, int *ppDev, int tileRow, int mtxRow)
+//1.matrix multipliction kernel function by using global memory access
+__global__ void mtxMultKernelGlobal(int *pmDev, int *pnDev, int *ppDev, int tileRow, int mtxRow)
 {
 	// thread id 
 	//   n
@@ -97,4 +78,29 @@ __global__ void mtxMultKernel(int *pmDev, int *pnDev, int *ppDev, int tileRow, i
 	ppDev[r*mtxRow + c]=0;
 	for(i=0; i<mtxRow; i++)
 		ppDev[r*mtxRow + c]+=pmDev[r*mtxRow + i]*pnDev[i*mtxRow + c];
+}
+
+//2 using shared memory
+__global__ void mtxMultKernel(int *pmDev, int *pnDev, int *ppDev, int tileRow, int mtxRow)
+{
+	//   n
+	// m p
+	__shared__ int pmShared[4][16]; //[mtxRow/tileRow][mtxRow];
+	__shared__ int pnShared[16][4]; //[mtxRow][mtxRow/tileRow];
+	int c=blockIdx.x*tileRow+ threadIdx.x;
+	int r=blockIdx.y*tileRow+ threadIdx.y;
+	int temp, i;
+	// each thread load mtxRow/tileRow byte of data into shared memory
+	for(temp=0; temp < mtxRow/tileRow; temp++){
+		pmShared[threadIdx.y][threadIdx.x*mtxRow/tileRow+temp]=pmDev[r*mtxRow + temp+threadIdx.x*mtxRow/tileRow];
+		pnShared[threadIdx.y*mtxRow/tileRow+temp][threadIdx.x]=pnDev[mtxRow*(temp+threadIdx.y*mtxRow/tileRow) + c];
+	}
+	__syncthreads();
+//	ppDev[r*mtxRow + c] = pmShared[threadIdx.y][c];
+	// matrix multiplication
+	temp=0;
+	for(i=0; i<mtxRow; i++)
+		temp+=pmShared[threadIdx.y][i]*pnShared[i][threadIdx.x];
+	__syncthreads();
+	ppDev[r*mtxRow + c]=temp;
 }
